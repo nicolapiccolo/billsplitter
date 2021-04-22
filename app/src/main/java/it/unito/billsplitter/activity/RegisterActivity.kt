bf7580ce0ffd24f2c65baef61a3af73b7db7b026
Francesco Scagliusi <frank.sca98@gmail.com>
2021-04-22 20:41:16 +0200

    added register

---
+++ app/src/main/java/it/unito/billsplitter/activity/RegisterActivity.kt
@@ -0,0 +1,47 @@
+package it.unito.billsplitter.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.parse.ParseException
+import com.parse.ParseUser
+import it.unito.billsplitter.R
+import it.unito.billsplitter.model.User
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    fun Register(view : View){
+
+        val user = ParseUser()
+        user.setUsername(txtName.text.toString())
+        user.setPassword(txtPassword.text.toString())
+        user.setEmail(txtEmail.text.toString())
+        user.signUpInBackground() { e: ParseException? ->
+            if(e == null){
+                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, e?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun Login(view: View){
+        intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun Back(view: View){
+        intent = Intent(this, LandingActivity::class.java)
+        startActivity(intent)
+    }
+}
