5c3c1e9d367e8b5cdec383f8a1f309a764c42b7f
Nicola Piccolo <nikola9piccolo9@gmail.com>
2021-04-23 11:04:12 +0200

    Aggiunta query split

--- app/src/main/java/it/unito/billsplitter/activity/MainActivity.kt
+++ app/src/main/java/it/unito/billsplitter/activity/MainActivity.kt
@@ -4,15 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.parse.Parse
+import androidx.recyclerview.widget.LinearLayoutManager
 import it.unito.billsplitter.R
+import it.unito.billsplitter.RvAdapter
 import it.unito.billsplitter.model.Model
 import it.unito.billsplitter.model.Split
 import it.unito.billsplitter.model.User
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),CellClickListener {
+
+    private lateinit var adapter: RvAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,16 +27,33 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
         else{
-            println(User.username)
+            setContentView(R.layout.activity_main)
+
             Toast.makeText(baseContext,"Welcome back ${User.username}", Toast.LENGTH_LONG).show()
 
-            val s : Split = Model.instance.getSplitElement(0)
-            println(s)
+            recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL ,false)
 
-            //txtSplit.text = s.name
-            setContentView(R.layout.activity_main)
+            populateList(Model.instance.getAllSplit())
+
+            //print("DATA: " + Model.instance.getSplit())
+
         }
     }
 
+    override fun onCellClickListener(data: Split) {
+        //Model.instance.getTotalofSplit("")
+        Toast.makeText(baseContext,"Card Click", Toast.LENGTH_LONG).show()
+    }
 
+
+    private fun populateList(list: ArrayList<Split>){
+        adapter = RvAdapter(this, list)
+        recyclerView.adapter = adapter
+        adapter.notifyDataSetChanged()
+    }
+
+}
+
+interface CellClickListener {
+    fun onCellClickListener(data: Split)
 }
--- app/src/main/java/it/unito/billsplitter/model/Split.kt
+++ app/src/main/java/it/unito/billsplitter/model/Split.kt
@@ -1,29 +1,126 @@
 package it.unito.billsplitter.model
 
-import com.parse.ParseException
 import com.parse.ParseObject
 import com.parse.ParseQuery
-import com.parse.ParseUser
+import com.parse.ParseRelation
 
 
-class Split(var name: String, var total: Float, var id_user: String) {}
+class Split(var name: String, var total: Float, var date: String, var owner: String) {}
 
 class Model private constructor()   {
 
+    private var dataList : ArrayList<Split> = ArrayList()
+
+    fun getOtherSplit(mySplit: List<String>) {
+
+        val query = ParseQuery.getQuery<ParseObject>("Transaction")
+        query.whereEqualTo("id_user", User.getCurrentUser())
+
+        val queryList: List<ParseObject> = query.find()
+
+        queryList.forEach {
+
+
+            val splitObj = (it.get("id_split") as ParseRelation<*>).query.find().get(0)
+
+            println("mysplit1: " + mySplit.get(0))
+            println("splitobj: " + splitObj.objectId)
+            println("check: " + splitObj.objectId in mySplit)
+
+            if (!(splitObj.objectId in mySplit)) {
+                var split = Split("", 0f, "", "")
+
+                //getTotalofSplit(it)
+
+                println("other: "  + splitObj.get("name"))
+
+                split.name = getNameSplit(splitObj)
+                split.total = (it.get("share") as Double).toFloat()
+
+                dataList.add(split)
+
+            }
+
+        }
+
+    }
+
+    fun getAllSplit(): ArrayList<Split> {
+        val mySplit = getMySplit()
+        getOtherSplit(mySplit)
+
+        return dataList
+    }
+
+    fun getMySplit(): List<String>{
+        //var dataList : ArrayList<Split> = ArrayList()
+
+
+        var listId = ArrayList<String>()
+        val query = ParseQuery.getQuery<ParseObject>("Split")
+        query.whereEqualTo("id_user", User.getCurrentUser())
+
+
+
+        val queryList: List<ParseObject> = query.find()
+
+
+        queryList.forEach{
+            var split = Split("", 0f, "", "")
+
+
+            //getTotalofSplit(it)
+            split.name = it.get("name") as String
+
+            split.total =  getTotalofMySplit(it)
+
+            listId.add(it.objectId)
+            dataList.add(split)
+        }
+        return listId
+    }
+
+    fun getTotalofMySplit(id_split: ParseObject): Float{
+        var total: Float = 0f
+        val query = ParseQuery.getQuery<ParseObject>("Transaction")
+        query.whereEqualTo("id_split",id_split)
+        query.whereNotEqualTo("id_user",User.getCurrentUser())
+        query.whereEqualTo("paid",false)
+        val queryList: List<ParseObject> = query.find()
+
+        queryList.forEach{
+            total = total + (it.get("share")as Double).toFloat()
+        }
+
+        println("LIST: " + total)
+        return total
+    }
+
+
+    fun getNameSplit(id_split: ParseObject): String{
+        val query = ParseQuery.getQuery<ParseObject>("Split")
+        query.whereEqualTo("objectId",id_split.objectId)
+
+        return query.find().get(0).get("name").toString()
+    }
+
+
     fun getSplitElement(position: Int) : Split {
-        var split = Split("",0f,"")
+        var split = Split("", 0f, "", "")
         val query = ParseQuery.getQuery<ParseObject>("Split")
         query.whereEqualTo("id_user", User.getCurrentUser())
 
         query.findInBackground { splitList, e ->
             if(e==null){
-                println("LIST: ")
-                println(splitList.get(position).objectId)
-                println(splitList.get(position).get("name"))
-                println(splitList.get(position).get("total"))
+                println("Get split element: " + splitList.get(position).objectId)
+                //println(splitList.get(position).get("name"))
+                //println(splitList.get(position).get("total"))
+                //println(splitList.get(position).get("created_at"))
+
 
                 split.name = splitList.get(position).get("name") as String
-                split.total = splitList.get(position).get("total") as Float
+                split.total = (splitList.get(position).get("total") as Double).toFloat()
+                split.date = (splitList.get(position).get("createdAt") as? String).toString()
             }else{
                 println(e.message)
             }
