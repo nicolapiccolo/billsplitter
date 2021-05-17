7adf87ee7294c8c3eb3a1a416bc18da8aa5cd657
Nicola Piccolo <nikola9piccolo9@gmail.com>
2021-05-17 17:17:48 +0200

    Fix view holder

--- app/src/main/java/it/unito/billsplitter/RvAdapterDetail.kt
+++ app/src/main/java/it/unito/billsplitter/RvAdapterDetail.kt
@@ -11,44 +11,34 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
 import it.unito.billsplitter.activity.CellClickListenerDetail
-import it.unito.billsplitter.activity.CellClickListener
 import it.unito.billsplitter.model.Contact
 import it.unito.billsplitter.model.SplitMember
 
-
 class RvAdapterDetail(private val cellClickListener: CellClickListenerDetail, private val dataList: ArrayList<SplitMember>) : RecyclerView.Adapter<RvAdapterDetail.ViewHolder>() {
-
     private lateinit var context: Context
-
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
         val v = LayoutInflater.from(p0?.context).inflate(R.layout.fragment_split_user, p0, false)
         context = p0.context
         return ViewHolder(v);
     }
-
     override fun getItemCount():Int{
-
         Log.d("SIZE", "Size: " + dataList.size)
         return dataList.size
-
     }
-
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-
         val data = dataList.get(p1)
 
 
-        if(!data.owner){
+        if(!data.owner) {
             p0.itemView.setOnLongClickListener {
+                //cellClickListener.onCellClickListener(data)
                 cellClickListener.onCellClickListener(data)
                 true
             }
-
+        }
 
         p0.name?.text = data.name.capitalize()
         p0.share?.text = data.share
@@ -70,12 +60,10 @@ class RvAdapterDetail(private val cellClickListener: CellClickListenerDetail, pr
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
         val name = itemView.findViewById<TextView>(R.id.m_txtName)
         val share = itemView.findViewById<TextView>(R.id.m_txtShare)
         val img = itemView.findViewById<TextView>(R.id.icon_text)
         val icon = itemView.findViewById<ImageView>(R.id.icon)
     }
-
 
 }
